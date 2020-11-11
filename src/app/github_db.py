c7d0efad04d07eb26a350964cb9405eae934b71e
Gloryness <gravity50ml@gmail.com>
2020-11-11 02:41:50 +0000

    Runnable with python code now (i think, idk)

--- src/app/github_db.py
+++ src/app/github_db.py
@@ -1,15 +1,22 @@
 import json
+import os
 from github import Github
 
 class GithubDatabase:
-    # If someone knows how to do this: Allowing the class to be used but not the code to be viewed, then dm me on discord - Gloryness#4341
-    # Would really appreciate it!
     def __init__(self, path: str):
         self.path = path
-        raise NotImplementedError("No usable implementation found!")
+        self.github = Github(os.environ.get('GITHUB_KEY'))
+        self.repo = self.github.get_user().get_repo('tassomai-automation')
 
     def get_content(self):
-        raise NotImplementedError("No usable implementation found!")
+        dataStr = self.repo.get_contents(self.path).decoded_content.decode('utf-8').strip()
+        data = json.loads(dataStr)
+        return data
 
     def edit_content(self, message, new_content):
-        raise NotImplementedError("No usable implementation found!")
+        new_content = json.dumps(new_content, indent=3)
+        if len(str(new_content)) < len(str(self.get_content())):
+            return
+        file = self.repo.get_contents(self.path)
+        data = self.repo.update_file(self.path, message, new_content, file.sha)
+        return data
